da2da3d9ed4bf7a2a180a0c83adc072b1b1242b9
TuanDao2002 <s3877347@rmit.edu.vn>
2022-10-27 17:02:38 +0700

    update

--- week1/app/src/main/java/rmit/ad/week1/MainActivity.java
+++ week1/app/src/main/java/rmit/ad/week1/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button0,
-            buttonC, buttonDel;
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button0, buttonC, buttonDel,
+            buttonAdd, buttonMinus, buttonMultiply, buttonDivide, buttonEqual;
 
-    TextView input;
+    TextView input, output;
+    boolean add, minus, multiply, divide;
+    int firstNum = -1, secondNum = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +34,14 @@ public class MainActivity extends AppCompatActivity {
         button9 = (Button) findViewById(R.id.button9);
         buttonC = (Button) findViewById(R.id.buttonC);
         buttonDel = (Button) findViewById(R.id.buttonDel);
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        buttonEqual = (Button) findViewById(R.id.buttonEqual);
 
         input = (TextView) findViewById(R.id.input);
+        output = (TextView) findViewById(R.id.output);
 
         button0.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,7 +126,14 @@ public class MainActivity extends AppCompatActivity {
         buttonC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                add = false;
+                minus = false;
+                multiply = false;
+                divide = false;
+                firstNum = 0;
+                secondNum = 0;
                 input.setText("");
+                output.setText("");
             }
         });
 
@@ -128,6 +144,92 @@ public class MainActivity extends AppCompatActivity {
                 if (inputTxt.length() == 0) return;
                 String txt = inputTxt.substring(0, inputTxt.length() - 1);
                 input.setText(txt);
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!add && !minus && !multiply && !divide) {
+                    firstNum = Integer.parseInt(input.getText().toString());
+                }
+                add = true;
+                minus = false;
+                multiply = false;
+                divide = false;
+                input.setText("");
+            }
+        });
+
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!add && !minus && !multiply && !divide) {
+                    firstNum = Integer.parseInt(input.getText().toString());
+                }
+                minus = true;
+                add = false;
+                multiply = false;
+                divide = false;
+                input.setText("");
+            }
+        });
+
+        buttonMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!add && !minus && !multiply && !divide) {
+                    firstNum = Integer.parseInt(input.getText().toString());
+                }
+                multiply = true;
+                add = false;
+                minus = false;
+                divide = false;
+                input.setText("");
+            }
+        });
+
+        buttonDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!add && !minus && !multiply && !divide) {
+                    firstNum = Integer.parseInt(input.getText().toString());
+                }
+                divide = true;
+                add = false;
+                minus = false;
+                multiply = false;
+                input.setText("");
+            }
+        });
+
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                secondNum = Integer.parseInt(input.getText().toString());
+                if (secondNum == -1) {
+                    output.setText("Second number missing");
+                    return;
+                }
+
+                output.setText("");
+                int result = 0;
+                if (add) {
+                    result = firstNum + secondNum;
+                } else if (minus) {
+                    result = firstNum - secondNum;
+                } else if (multiply) {
+                    result = firstNum * secondNum;
+                } else if (divide) {
+                    if (secondNum == 0) {
+                        output.setText("divide by 0");
+                        return;
+                    }
+                    result = firstNum / secondNum;
+                }
+                
+                output.setText(String.valueOf(result));
+                firstNum = result;
             }
         });
     }
